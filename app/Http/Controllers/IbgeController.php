ba282ee8e9a28c66b3cda737a04af6150bc7bdea
LucasLm2 <lucas_pss13@hotmail.com>
2023-01-20 17:52:15 -0300

    Fim da documentação

--- app/Http/Controllers/IbgeController.php
+++ app/Http/Controllers/IbgeController.php
@@ -10,7 +10,6 @@ use App\Repository\Contracts\IIbge;
  *     version="0.1",
  *     title="API IBGE BCode"
  * )
- * @OA\PathItem(path="/api/ibge")
  */
 class IbgeController extends Controller
 {
@@ -19,33 +18,39 @@ class IbgeController extends Controller
 
     /**
      * @OA\Get(
-     *      path="api/v1/ibge/municipios/uf/{uf}",
+     *      path="/api/v1/ibge/municipios/uf/{uf}",
+     *      @OA\Parameter(
+     *         name="uf",
+     *         in="path",
+     *         description="Buscar por estado",
+     *         required=true,
+     *      ),
      *      @OA\Response(
      *          response="200", 
      *          description="Sucesso",
      *          @OA\JsonContent(
-     *              @OA\Examples(example="result", value={"success": true}, summary="json")
+     *              @OA\Examples(example="result", value={{"name": "Abatiá","ibge_code": 4100103},{"name": "Agudos do Sul","ibge_code": 4100301},{"name": "Almirante Tamandaré","ibge_code": 4100400},}, summary="json")
      *          )
      *      ),
      *      @OA\Response(
      *          response="404", 
      *          description="Não Encontrado",
      *          @OA\JsonContent(
-     *              @OA\Examples(example="result", value={"success": true}, summary="json")
+     *              @OA\Examples(example="result", value={"message": "Não encontrado."}, summary="json")
      *          )
      *      ),
      *      @OA\Response(
      *          response="422", 
      *          description="Erro",
      *          @OA\JsonContent(
-     *             @OA\Examples(example="result", value={"success": true}, summary="json")
+     *             @OA\Examples(example="result", value={"message": "O campo uf não é um UF válido.","errors": {"uf": {"O campo uf não é um UF válido."}}}, summary="json")
      *          )
      *      ),
      *      @OA\Response(
      *          response="500", 
      *          description="Erro",
      *          @OA\JsonContent(
-     *             @OA\Examples(example="result", value={"success": true}, summary="json")
+     *             @OA\Examples(example="result", value={"message": "Server Error"}, summary="json")
      *          )
      *      )
      * )
