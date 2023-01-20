2b99f80e7f7848fe39a4358db8026e18c61358e3
LucasLm2 <lucas_pss13@hotmail.com>
2023-01-20 13:17:04 -0300

    Documentando com Swagger

--- app/Http/Controllers/IbgeController.php
+++ app/Http/Controllers/IbgeController.php
@@ -5,13 +5,51 @@ namespace App\Http\Controllers;
 use App\Http\Requests\MunicipiosPorUfRequest;
 use App\Repository\Contracts\IIbge;
 
-
+/**
+ * @OA\Info(
+ *     version="0.1",
+ *     title="API IBGE BCode"
+ * )
+ * @OA\PathItem(path="/api/ibge")
+ */
 class IbgeController extends Controller
 {
     public function __construct(private IIbge $ibge)
     {}
 
-    
+    /**
+     * @OA\Get(
+     *      path="api/v1/ibge/municipios/uf/{uf}",
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Sucesso",
+     *          @OA\JsonContent(
+     *              @OA\Examples(example="result", value={"success": true}, summary="json")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Não Encontrado",
+     *          @OA\JsonContent(
+     *              @OA\Examples(example="result", value={"success": true}, summary="json")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="422", 
+     *          description="Erro",
+     *          @OA\JsonContent(
+     *             @OA\Examples(example="result", value={"success": true}, summary="json")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="500", 
+     *          description="Erro",
+     *          @OA\JsonContent(
+     *             @OA\Examples(example="result", value={"success": true}, summary="json")
+     *          )
+     *      )
+     * )
+     */
     public function listarMunicipiosPorUf(MunicipiosPorUfRequest $request)
     {
         $resultadoConsulta = $this->ibge->listarMunicipiosPorUf($request->uf);
